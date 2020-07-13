48ecff676c35f73a2b89bf2b2b239441aeb4e9bc
Melanie Tran <melaniexx02@gmail.com>
2020-07-12 21:25:22 -0600

    Remove dependent destroy; didn't help with passing spec_harness

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,6 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
 
-  has_many :invoices, dependent: :destroy
-  has_many :items, dependent: :destroy
+  has_many :invoices
+  has_many :items
 end
