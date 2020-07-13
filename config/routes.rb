083a08c9571a7fdbd81ae81dec748e4b6ac8001c
Melanie Tran <melaniexx02@gmail.com>
2020-07-12 22:21:58 -0600

    Add route to find merchant for item

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
         get '/:id/items', to: 'items#index'
       end
 
+      namespace :items do
+        get '/:id/merchant', to: 'merchants#show'
+      end
+
       resources :merchants, except: [:new, :edit]
       resources :items, except: [:new, :edit]
     end
