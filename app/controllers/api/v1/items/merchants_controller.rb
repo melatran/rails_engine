8b337dfb95c8cf7b6d46c4dcf557db77aa2c4534
Melanie Tran <melaniexx02@gmail.com>
2020-07-12 22:33:30 -0600

    Delete unused variable

--- app/controllers/api/v1/items/merchants_controller.rb
+++ app/controllers/api/v1/items/merchants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Items::MerchantsController < ApplicationController
   def show
     merchant_id = Item.find(params[:id]).merchant_id
-    bob =render json: MerchantSerializer.new(Merchant.find_by(id: merchant_id))
+    render json: MerchantSerializer.new(Merchant.find_by(id: merchant_id))
   end
 end
