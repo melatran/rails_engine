a5f83dcce6fa4901279eaa4a5bb4d8abb0dceb73
Melanie Tran <melaniexx02@gmail.com>
2020-07-12 21:40:51 -0600

    Move around routes with namespace merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, except: [:new, :edit] do
-        resources :items, only: [:index]
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
       end
 
+      resources :merchants, except: [:new, :edit]
       resources :items, except: [:new, :edit]
     end
   end
