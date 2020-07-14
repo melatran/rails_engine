c9f0bfe0794b61ba0fef77c194f06064124f831b
Melanie Tran <melaniexx02@gmail.com>
2020-07-13 20:57:18 -0600

    Create total_revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,15 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
   has_many :transactions, through: :invoices
+
+  def total_revenue
+    invoices
+      .joins(:transactions)
+      .where(transactions: { result: 'success' })
+      .joins(:invoice_items)
+      .sum('invoice_items.quantity * invoice_items.unit_price')
+    #start with invoices and transactions
+    #transaction need to have result success to be paid invoice
+    #total_revnue = quantity * unit_price (only invoice item table has relationship)
+  end
 end
