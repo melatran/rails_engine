6d8d45183792e0c2543c041266cc0680140a7635
Melanie Tran <melaniexx02@gmail.com>
2020-07-13 21:27:06 -0600

    Add route to the revenue controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         get '/:id/items', to: 'items#index'
+        get '/:id/revenue', to: 'revenue#show'
       end
 
       namespace :items do
