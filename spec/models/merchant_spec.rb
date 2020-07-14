9100bd3a9c8f3e7a970fbc214840e9ed68ce11e3
Melanie Tran <melaniexx02@gmail.com>
2020-07-13 20:39:45 -0600

    Write test for merchant's total revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,4 +10,26 @@ describe Merchant do
     it {should have_many :items}
     it {should have_many(:transactions).through(:invoices)}
   end
+
+  describe "instance_methods" do
+    it ".total_revenue" do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+
+      item_1 = create(:item, merchant_id: merchant_1.id, unit_price: 10.00)
+      item_2 = create(:item, merchant_id: merchant_2.id, unit_price: 5.50)
+      item_3 = create(:item, merchant_id: merchant_2.id, unit_price: 25.00)
+
+      invoice_1 = create(:invoice, merchant_id: merchant_1.id)
+
+      invoice_item_1 = create(:invoice_item, invoice_id: invoice_1.id, item_id: item_1.id, quantity: 2, unit_price: 10.00)
+      invoice_item_1 = create(:invoice_item, invoice_id: invoice_1.id, item_id: item_2.id, quantity: 1, unit_price: 5.50)
+
+      transaction = create(:transaction, invoice_id: invoice_1.id, result: 'success')
+
+      expect(merchant_1.total_revenue).to eq(25.50)
+      expect(merchant_1.total_revenue).to_not eq(40.50)
+
+    end
+  end
 end
