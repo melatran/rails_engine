a9e0c510f78757eda47551825ffc836ec29fe47e
Melanie Tran <melaniexx02@gmail.com>
2020-07-15 16:11:19 -0600

    Update merchant model methds for most revenue and most items

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,21 +13,21 @@ class Merchant < ApplicationRecord
       .sum('invoice_items.quantity * invoice_items.unit_price')
   end
 
-  def self.most_revenue(quantity = nil)
+  def self.most_revenue(limit)
     select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
-      .joins(invoices: [:transactions, :invoice_items])
+      .joins(invoices: [:invoice_items, :transactions])
       .merge(Transaction.successful)
-      .group('merchants.id')
+      .group(:id)
       .order('revenue DESC')
-      .limit(quantity)
+      .limit(limit)
   end
 
-  def self.most_items_sold(quantity = nil)
+  def self.most_items_sold(limit)
     select('merchants.*, SUM(invoice_items.quantity) AS items_sold')
-      .joins(invoices: [:transactions, :invoice_items])
+      .joins(invoices: [:invoice_items, :transactions])
       .merge(Transaction.successful)
       .group('merchants.id')
       .order('items_sold DESC')
-      .limit(quantity)
+      .limit(limit)
   end
 end
