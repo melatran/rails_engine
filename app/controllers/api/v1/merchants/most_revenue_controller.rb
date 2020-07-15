589c76c975bfd3b0af6779e1b48ac10efa95112a
Melanie Tran <melaniexx02@gmail.com>
2020-07-14 21:55:09 -0600

    Add nil back to quantity to fix an error

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::MostRevenueController < ApplicationController
   def index
-    merchant = Merchant.most_revenue(quantity)
+    merchant = Merchant.most_revenue(quantity = nil)
     render json: MerchantSerializer.new(merchant)
   end
 end
