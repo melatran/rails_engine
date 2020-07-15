6fcb2a1f79295553901956ff8ba10e88a5f7a736
Melanie Tran <melaniexx02@gmail.com>
2020-07-14 21:58:52 -0600

    Add route for most_items

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
         get '/:id/items', to: 'items#index'
         get '/:id/revenue', to: 'revenue#show'
         get '/most_revenue', to: 'most_revenue#index'
+        get '/most_items', to: 'most_items#index'
       end
 
       namespace :items do
