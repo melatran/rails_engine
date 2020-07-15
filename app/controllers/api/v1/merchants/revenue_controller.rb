df7826c9a36ab1756337b3fdf922a60ab0caab0c
Melanie Tran <melaniexx02@gmail.com>
2020-07-14 19:38:24 -0600

    Remove unused variable

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
-    merchant = Merchant.find(params[:id])
     render json: RevenueSerializer.new(Merchant.find(params[:id]))
   end
 end
