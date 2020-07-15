a2d396e967cb0d0073dc0754566933504f9ea41d
Melanie Tran <melaniexx02@gmail.com>
2020-07-15 14:29:53 -0600

    Update tests to reflect transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,7 @@
 class Transaction < ApplicationRecord
+  validates :credit_card_number, presence: true
   validates :result, presence: true
+
   belongs_to :invoice
 
   scope :successful, -> { where(result: "success") }
