ba692a7dbab4be330a38fe26594dcd2bd7ab4444
Melanie Tran <melaniexx02@gmail.com>
2020-07-16 06:34:48 -0600

    Passing spec harness

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -14,6 +14,8 @@ class Api::V1::Items::SearchController < ApplicationController
   def search_params(params)
     item_results = Item.all
     item_results = item_results.filter_by_name(params[:name]) if params[:name].present?
+    item_results = item_results.filter_by_id(params[:id]) if params[:id].present?
+    item_results = item_results.filter_by_merchant_id(params[:merchant_id]) if params[:merchant_id].present?
     item_results = item_results.filter_by_description(params[:description]) if params[:description].present?
     item_results = item_results.filter_by_created_at(params[:created_at]) if params[:created_at].present?
     item_results = item_results.filter_by_updated_at(params[:updated_at]) if params[:updated_at].present?
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,9 +5,10 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :transactions, through: :invoices
 
-  scope :filter_by_name, -> (name) { where('lower(name) LIKE ?', "#{name.downcase}%") }
-  scope :filter_by_created_at, -> (created_at) { where("Date(created_at) = ?", "#{created_at}") }
-  scope :filter_by_updated_at, -> (updated_at) { where("Date(updated_at) = ?", "#{updated_at}") }
+  scope :filter_by_name, -> (name) { where("lower(name) like ?", "%#{name.downcase}%")}
+  scope :filter_by_id, -> (id) { where id: id }
+  scope :filter_by_created_at, -> (created_at) { where("Date(created_at) = ?", "#{created_at}%")}
+  scope :filter_by_updated_at, -> (updated_at) { where("Date(updated_at) = ?", "#{updated_at}%") }
 
   def total_revenue
     invoices
